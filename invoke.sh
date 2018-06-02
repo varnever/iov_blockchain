7c37aa75291b3d254a85578b70ed10aad04d5438
varnever <39845228+varnever@users.noreply.github.com>
2018-06-02 09:37:32 +0800

    Create invoke.sh

---
+++ invoke.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+peer chaincode invoke -o orderer.example.com:7050  --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C mychannel -n mycc -c '{"Args":["invoke","'$1'","'$2'","'$3'"]}'

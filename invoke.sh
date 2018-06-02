8705381b5ffe19f9ee3aea4d7a7ce4600667a11f
varnever <39845228+varnever@users.noreply.github.com>
2018-06-02 10:05:15 +0800

    Update invoke.sh

--- invoke.sh
+++ invoke.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-peer chaincode invoke -o orderer.example.com:7050  --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C mychannel -n mycc -c '{"Args":["invoke","'$1'","'$2'","'$3'"]}'
+docker exec -it cli /bin/bash /home/Shell/invoke.sh $1 $2 $3 | grep successful | awk {'print $11'}
